f0d9161c552fe357435b0757cbf7dd64b9c71f15
nidronia <axel@apea.se>
2021-09-08 23:48:00 +0200

    commiting cpp folder

---
+++ cpp/calc/01calc.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string input;
+	std::cin >> input;
+}
---
+++ cpp/tetrisc/source.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+wstring tetromino[7];
+
+int main()
+{
+	// create assets
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+
+	/* Playing field */
+
+	pField = new unsigned char[nFieldWidth*nFieldHeight];
+	for (int y = 0; x < nFieldwidth; y++)
+		
+}
